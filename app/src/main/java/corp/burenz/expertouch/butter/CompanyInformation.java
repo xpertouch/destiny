5b1032b73f6c5ce8b81f959cbe20d5c67818be1a
xpertouch <mail.1clickaway@gmail.com>
2018-03-10 13:13:46 +0530

    resolving unversioned files phase 2

---
+++ app/src/main/java/corp/burenz/expertouch/butter/CompanyInformation.java
@@ -0,0 +1,23 @@
+package corp.burenz.expertouch.butter;
+
+import corp.burenz.expertouch.R;
+
+/**
+ * Created by buren on 13/12/17.
+ */
+
+public class CompanyInformation {
+
+
+
+    public  static String   COMPANY_DETAILS = "myCompanyDetails";
+
+    public  static String   VERIFICATION_STATUS = "CVERIFIED";
+
+
+
+
+
+
+
+}
